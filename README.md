51f2ca4a1aee6d1a50745e5720b6fa0847987072
Michael Yang <myang@viantinc.com>
2015-10-15 16:38:53 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# SparkyJava
+
+This is using Java 1.7 and SparkJava 1.1.1
+
+This is a demo for simple Web RESTful service.
+/helloworld
+/helloworld/{input name}
+/createAPost
+/listAllPosts
+
+~$ mvn clean install
+~$ mvn exec:java -Dexec.mainClass="com.myang.App"
+
+It will launch Jetty with port 4567  
---
+++ src/main/java/com/myang/App.java
@@ -0,0 +1,104 @@
+package com.myang;
+
+import static spark.Spark.*;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.google.gson.Gson;
+import com.myang.dao.PostDAO;
+import com.myang.dao.impl.PostDAOImpl;
+import com.myang.dto.NewPostPayloadDTO;
+import spark.Request;
+import spark.Response;
+import spark.Route;
+
+import java.io.IOException;
+import java.io.StringWriter;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+  private static final int HTTP_BAD_REQUEST = 400;
+
+  public static String dataToJson(Object data)
+  {
+    try
+    {
+      ObjectMapper mapper = new ObjectMapper();
+      mapper.enable(SerializationFeature.INDENT_OUTPUT);
+      StringWriter sw = new StringWriter();
+      mapper.writeValue(sw, data);
+
+      return sw.toString();
+    }
+    catch (IOException e)
+    {
+      throw new RuntimeException("IOException from a StringWriter?");
+    }
+  }
+
+  public static void main(String[] args)
+  {
+    System.out.println("Hello World!");
+    //lambda expression for Java 1.8
+    //get("/hello", (req, res) -> "Hello World");
+
+    //for Java 1.7
+    get(new Route("/helloworld")
+    {
+      public Object handle(Request request, Response response) {
+        return "Hello World!";
+      }
+    });
+
+    get(new Route("/helloworld/:username")
+    {
+      public Object handle(Request request, Response response)
+      {
+        return "Hello " + request.params("username") + "!";
+      }
+    });
+
+    //Data persists in Memory.  There is no DB involved for now
+    PostDAO postDao = new PostDAOImpl();
+    Gson gson = new Gson();
+
+    // insert a post (using HTTP post method)
+    post(new Route("/createAPost") {
+      public Object handle(Request request, Response response) {
+        try {
+          ObjectMapper mapper = new ObjectMapper();
+          NewPostPayloadDTO newPostData = mapper.readValue(request.body(), NewPostPayloadDTO.class);
+          if (!newPostData.isValid()) {
+            response.status(HTTP_BAD_REQUEST);
+            return "";
+          }
+          int id = postDao.createPost(newPostData.getTitle(), newPostData.getContent(), newPostData.getCategories());
+          response.status(200);
+          response.type("application/json");
+
+          return gson.toJson(newPostData);
+        }
+        catch (IOException ex)
+        {
+          response.status(HTTP_BAD_REQUEST);
+          return "Request Body Does Not Match with DTO object";
+        }
+      }
+
+    });
+
+    // get all post (using HTTP get method)
+    get(new Route("/listAllPosts") {
+      public Object handle(Request request, Response response) {
+        response.status(200);
+        response.type("application/json");
+        return dataToJson(postDao.getAllPostsInList());
+      }
+    });
+
+  }
+}
